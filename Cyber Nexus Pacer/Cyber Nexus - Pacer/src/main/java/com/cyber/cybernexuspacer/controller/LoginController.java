58ba40b7313325d46369fa45e61b2e43a998a554
Diego Sales <diegosgjoao@gmail.com>
2024-12-01 15:51:27 -0300

    criado texto de esqueci minha senha na view e controller

--- Cyber Nexus Pacer/Cyber Nexus - Pacer/src/main/java/com/cyber/cybernexuspacer/controller/LoginController.java
+++ Cyber Nexus Pacer/Cyber Nexus - Pacer/src/main/java/com/cyber/cybernexuspacer/controller/LoginController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -26,6 +27,9 @@ public class LoginController {
     public Label lblTxtSistemaPacer;
     @FXML
     private Button btnEntrar;
+
+    @FXML
+    private Text esqueciMinhaSenha;
 
     @FXML
     private ImageView imgFatec;
